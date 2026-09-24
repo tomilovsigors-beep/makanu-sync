e812f7b9002742c1fcc286ccb926731441a5205f
tomilovsigors-beep <tomilovsigors@gmail.com>
2026-09-25 00:25:43 +0300

    Fix Makanu login form field detection

--- app.py
+++ app.py
@@ -31,10 +31,18 @@ async def open_logged_in_page():
 
     password_field = page.locator('input[type="password"]').first
     if await password_field.count():
-        login_field = page.locator(
-            'input[type="email"], input[name*="login" i], input[name*="user" i], '
-            'input[name*="email" i], input[type="text"]'
-        ).first
+        form = password_field.locator("xpath=ancestor::form[1]")
+        login_field = form.locator(
+            'input:not([type="password"]):not([type="hidden"]):not([type="submit"]):not([type="button"])'
+        ).filter(visible=True).first
+
+        if not await login_field.count():
+            login_field = page.locator(
+                'input[type="email"]:visible, '
+                'input[name*="login" i]:visible, '
+                'input[name*="user" i]:visible, '
+                'input[name*="email" i]:visible'
+            ).first
 
         if not await login_field.count():
             await browser.close()
